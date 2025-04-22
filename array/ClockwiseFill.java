b5f69f8937837dab83e8a30f920c97c4c40ae1eb
Sagnik Chatterjee <97253300+sagn-ik@users.noreply.github.com>
2025-04-22 23:34:17 +0530

    Create ClockwiseFill.java

---
+++ array/ClockwiseFill.java
@@ -0,0 +1,38 @@
+// Program to fill elements in clockwise order (from outside to inside) in a 2D array
+
+import java.util.*;
+public class Clock
+{
+    public static void main()
+    {
+        Scanner sc=new Scanner (System.in);
+        int temp;
+        System.out.println("Enter array size: ");
+        int n=sc.nextInt();
+        int arr[][]=new int[n][n];
+        int clk[][]=new int[n][n];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+               System.out.print("Enter no: ");
+               arr[i][j]=sc.nextInt(); 
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+               clk[i][j]=arr[n-j-1][i];
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                System.out.print(clk[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
